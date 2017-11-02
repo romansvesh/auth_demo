69783bc72820196ba8d12278081376d44fdf9707
Roman Sveshnikov <romansvesh@gmail.com>
2017-11-02 09:35:17 +0400

    Сделал константу приватной в рамках модуля

--- page/main_page.py
+++ page/main_page.py
@@ -3,9 +3,9 @@ from selenium.webdriver.support.expected_conditions import visibility_of_element
 
 from helpers.helper import wait_for_element
 
-LOGOUT_BUTTON = (By.LINK_TEXT, 'Logout')
+__LOGOUT_BUTTON = (By.LINK_TEXT, 'Logout')
 
 
 def get_logout_button_presence(driver):
-    wait_for_element(LOGOUT_BUTTON, driver)
-    return visibility_of_element_located(LOGOUT_BUTTON)
+    wait_for_element(__LOGOUT_BUTTON, driver)
+    return visibility_of_element_located(__LOGOUT_BUTTON)
